06b85c4bd89e91422fd334b5becf9e4cfb4b888e
Abrhamtaye7 <abrhamtayetessfaye@gmail.com>
2026-01-18 20:48:29 +0300

    fix 1

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.http import HttpResponse
 
 
 schema_view = get_schema_view(
@@ -30,11 +31,12 @@ schema_view = get_schema_view(
     public=True,
     permission_classes=[permissions.AllowAny],
 )
-
+def home_view(request):
+    return HttpResponse("<h1>Welcome to my API Deployment!</h1><p>Go to <a href='/swagger/'>/swagger/</a> for docs.</p>")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-
+    path("", home_view),
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
 ]
